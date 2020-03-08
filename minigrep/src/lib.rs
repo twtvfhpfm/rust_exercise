c6c56fd92999eea3bf9569fa0c7d72c67e873dda
Xu Jiannan <xu_jiannan@126.com>
2020-03-08 22:37:38 +0800

    use iterator

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -10,14 +10,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str>
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str>
     {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(s) => s,
+            None => return Err("query is none")
+        };
+        let filename = match args.next() {
+            Some(s) => s,
+            None => return Err("filename is none")
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config {query, filename, case_sensitive})
@@ -40,24 +44,12 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query){
-            result.push(line);
-        }
-    }
-    result
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
     let query = query.to_lowercase();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query){
-            result.push(line);
-        }
-    }
-    result
+    contents.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
 }
 
 #[cfg(test)]
