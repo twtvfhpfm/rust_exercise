a30f097065e172e7b7482b7a4798f1f345728ba6
Xu Jiannan <xu_jiannan@126.com>
2020-06-03 08:00:44 +0800

    optimize webserver

--- webserver/src/bin/main.rs
+++ webserver/src/bin/main.rs
@@ -7,7 +7,7 @@ use webserver::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:5876").unwrap();
     let pool = ThreadPool::new(4);
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
         pool.execute(||{
             handle_connection(stream);
--- webserver/src/lib.rs
+++ webserver/src/lib.rs
@@ -5,9 +5,14 @@ use std::sync::Mutex;
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+enum Message {
+    NewJob(Job),
+    Teminate,
+}
+
 pub struct ThreadPool{
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>
+    sender: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
@@ -28,22 +33,45 @@ impl ThreadPool {
     pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self){
+        for _ in &self.workers {
+            self.sender.send(Message::Teminate).unwrap();
+        }
+        for worker in &mut self.workers {
+            println!("shut down worker({})", worker.id);
+
+            if let Some(thread) = worker.thread.take(){
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker{
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker{
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("worker {} got a job, executing", id);
-            job();
+            let message = receiver.lock().unwrap().recv().unwrap();
+            match message {
+                Message::NewJob(job) => {
+                    println!("worker {} got a job, executing", id);
+                    job();
+                },
+                Message::Teminate => {
+                    println!("worker {} was told to terminate", id);
+                    break;
+                }
+            }
         });
-        Worker{id, thread}
+        Worker{id, thread: Some(thread)}
     }
 }
